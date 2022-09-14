2e2a95ce5617de5d0a72cc1cfcfaa70e01d4dd43
Paulo Bruny Lima <paulobruny@gmail.com>
2022-09-13 22:23:22 -0300

    feat: set map to show on the screen and link css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,10 +11,16 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
+          integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
+          crossOrigin=""
+        />
         <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="A project to practice next js and many other technologies and improve my abilities."
         />
       </Head>
       <GlobalStyles />
